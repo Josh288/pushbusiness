d3105b82af74e91d7bcb838ddfec7fc54a4af238
samantazeledon04 <137316276+samantazeledon04@users.noreply.github.com>
2023-10-20 09:08:18 -0600

    Registrolisto

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,109 +1,152 @@
 
 
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-    </script>
-
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
     @section('content')
-        <div class="container text-center ">
-            <div class="row justify-content-right">
-                {{-- <div class="container text-center border:primary"> --}}
-                <div class="cold-md-8 text-center>
+           <!-- Section: Design Block -->
+<section class="text-center text-lg-start">
+    <style>
 
+     body
+     {
+        font-family: "Nunito Sans", sans-serif;
+     }
 
+      .form-outline
+      {
+        font-family: "Nunito Sans", sans-serif;
+        font-size: 22px;
+        color: rgb(255, 255, 255);
+      }
 
-            <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <img class="rounded-circle" src="../assets/images/user.png" height="150px" width="150px">
-                        <h4></h4>
-                        <div class="container text-center col-md-3 py-2>
-                          </div>
+      .fw-bold
+      {
+        color: rgb(255, 255, 255);
+      }
+      .btn
+      {
+        font-size: 22px;
+        color: rgb(255, 255, 255);
+      }
 
+      .btn-primary
+      {
+        background-color:  #006F76;
+        border-block-color: #006F76;
+        border-block-end-color: #006F76;
+        border-block-start-color: #006F76
+      }
 
-        {{-- }} Name --}}
-        <div>
-            <x-input-label for="name"
-                            :value="__('Nombre')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
-                            :value="old('name')" required autofocus autocomplete="name" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+      .cascading-right {
+        margin-right: -60px;
+      }
+
+      @media (max-width: 991.98px) {
+        .cascading-right {
+          margin-right: 0;
+        }
+      }
+    </style>
+
+    <!-- Jumbotron -->
+    <div class="container py-6">
+      <div class="row g-0 align-items-center">
+        <div class="col-lg-8 mb-2 mb-lg-0">
+          <div class="card cascading-right" style="
+              background: hsla(180, 69%, 29%, 0.767);
+              backdrop-filter: blur(30px);
+              ">
+              <form method="POST">
+                @csrf
+
+            <div class="card-body p-6 shadow-5 text-center">
+              <h2 class="fw-bold mb-3">{{ __('Registrarse') }}</h2>
+              <form>
+                <!--Nombres del usuario nuevo -->
+                <div class="row">
+                  <div class="form-outline mb-3">
+                      <input type="name" id="form3Example1" class="form-control  @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                      @error('name')
+                      <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                      @enderror
+                      <label class="form-label" for="form3Example1">{{ __('Nombres') }}</</label>
+                   </div>
+                  </div>
+
+                <!-- Correo del usuario -->
+                <div class="form-outline mb-3">
+                  <input type="mail" id="form3Example4" class="form-control" @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email""
+                  required autocomplete="username">
+                  @error('email')
+                 <span class="invalid-feedback" role="alert">
+                 <strong>{{ $message }}</strong>
+                 @enderror
+                  <label class="form-label" for="form3Example4">{{ __('Correo') }} </</label>
                 </div>
 
-                <!-- Email Address -->
-                <div class="mt-4">
-                    <x-input-label for="email" :value="__('Correo')" />
-                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                        required autocomplete="username" />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                </div>
+                <!-- Contraseña -->
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                      <div class="form-outline">
+                        <input type="password" id="form3Example1" class="form-control" @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" />
+                        @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                        @enderror
+                        <label class="form-label" for="form3Example1">{{ __('Contraseña') }} </</label>
+                      </div>
+                    </div>
 
-                <!-- Password -->
-                <div class="mt-4">
-                    <x-input-label for="password" :value="__('Contraseña')" />
+                    <!-- Confirmar contraseña -->
+                    <div class="col-md-6 mb-4">
+                      <div class="form-outline" >
+                        <input type="password" id="form3Example2" class="form-control" name="password_confirmation" required autocomplete="new-password"  />
+                        @error('age')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                        <label class="form-label" for="form3Example2">{{ __('Confirmar contraseña') }} </</label>
+                      </div>
+                    </div>
+                  </div>
 
-                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                        autocomplete="new-password" />
+                  <!-- Edad -->
+                <div class="row">
+                    <div class="form-outline mb-4 ">
+                        <input type="number" id="form3Example1" class="form-control  min="18" max="99" name="age"/>
+                        <label class="form-label" for="form3Example1">{{ __('Edad') }}</</label>
+                     </div>
+                    </div>
 
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                </div>
-
-                <!-- Confirm Password -->
-                <div class="mt-4">
-                    <x-input-label for="password_confirmation" :value="__('Confirmar contraseña')" />
-
-                    <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                        name="password_confirmation" required autocomplete="new-password" />
-
-                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                </div>
-
-
-                <!-- Edad. Mayor a 18 años -->
-
-                <div class="mt-4">
-                    <x-input-label for="age" :value="__('Edad')" />
-                    <x-text-input id="age" class="block mt-1 w-medium" type="number" min="18" max="99"
-                        name="age" />
-                </div>
-
-
-
-                {{--  <input type="radio" name="usuario" value="1">
+                                    {{--  <input type="radio" name="usuario" value="1">
             <input type="radio" name="usuario" value="2"> --}}
 
 
 
-                <div class="flex items-center justify-center mt-4">
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        href="{{ route('login') }}">
-                        {{ __('¿Ya tienes cuenta? Inicia sesión') }}
-                    </a>
+                <!-- Boton de inicio -->
+                <button type="submit" class="btn btn-primary btn-ligth btn-block mb-4">
+                                    {{ __('Registrarse') }}
+                </button>
 
-                    <x-primary-button class="ml-4">
-                        {{ __('Register') }}
-                    </x-primary-button>
+                <div class="form-outline mb-3">
+                    <a class="btn form-control" href="{{ route('login') }}">
+                        {{ __('¿Ya tienes alguna cuenta? Inicia sesión') }}
+                    </a>
                 </div>
 
-                </form>
-
-
-
-
-
-
-
-
-
-
-
+              </form>
+            </div>
+          </div>
         </div>
-
-
+        <div class="col-lg-4 mb-2 mb-lg-0">
+          <img src="../assets/img/log.png" class="w-100 rounded-4 shadow-4 "
+            alt="" />
         </div>
-
+      </div>
+    </div>
+    <!-- Jumbotron -->
+  </section>
